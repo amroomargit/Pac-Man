a04dca3c95745c8d1dd56bec29af526dbeb89fcd
Amro Omar <amroomargit@gmail.com>
2024-11-08 01:36:16 -0500

    Set up game window, implement basic game character logic

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception{
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("Pac Man"); //name of window and object definition
+        //frame.setVisible(true); //frame visibility
+        frame.setSize(boardWidth,boardHeight); //frame size
+        frame.setLocationRelativeTo(null); //frame center screen
+        frame.setResizable(false); //don't allow user to resize frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //close when x is clicked
+
+        PacMan pacmanGame = new PacMan();
+        frame.add(pacmanGame); //add panel to window
+        frame.pack(); //full size of JPanel in window
+        frame.setVisible(true);
+    }
+}
